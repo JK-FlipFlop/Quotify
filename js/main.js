52ae9274f00a9a351a2c02c46acda2e8909d421c
Jai Kapur <35732432+JK-FlipFlop@users.noreply.github.com>
2018-04-07 22:48:02 +0530

    Final Commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 $(document).ready(function()
 {
+	
+	getQuote();
+
+	
+});
+
+
 	function getQuote()
 	{
-		var quotes = ["Chal Rahe Hai", "Kahaan YAHAAN", "Thanks for Participating"];
-		var authors = ["- author1", "- author2", "- author3"];
+		var quotes = [ '"I aint no tailor, but I know what suits me."',
+						'"Maybe if i stare at this piece of paper long enough, people will think I can read!"',
+						'"I hate Mondays."',
+						'"Im normally not a praying man, but if youre up there, please save me Superman."',
+						 '"Me fail English? Thats unpossible."',
+						 '"Lets do what we do best Scoob, eat."',
+						 '"Hi, Dexter! Oooooo, what does this button do?"'];
+		var authors = ["- Popeye", "- Daffy Duck", "- Garfield","- Homer (The Simpsons)","- Ralph (The Simpsons)","- Shaggy (Scooby Doo)","- Dee Dee (Dexter's Laboratory)"];
 
 		var randomNumber = Math.floor((Math.random() * quotes.length));
 
@@ -12,10 +25,12 @@ $(document).ready(function()
 
 		$('#quote').text(randomQuote);
 		$('#author').text(randomAuthor);
-	}
 
-	$('.newQuote').on("click",function()
+		$('.newQuote').on("click",function()
 	{
 		getQuote();
-	});
-});
+	});	
+	}
+
+
+	
